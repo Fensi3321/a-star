2103d2c3429864c358f0c345abc3dd3171d669c5
bfs <barteksemeniuk57@gmail.com>
2020-09-30 02:45:12 +0200

    lol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-
+import grid as g
 w = 1080
 h = 720
 
@@ -18,22 +18,15 @@ def handle_events():
         if event.type == pg.QUIT:
             pg.quit()
 
-def draw_grid(surface: pg.Surface):
-    surface.fill((16,16,16))
-
-    for x in range(0, w, 54):
-        pg.draw.line(surface, (64,64,64), (x, 0), (x, h), 1)
-        for y in range(0, h, 54):
-            pg.draw.line(surface, (64,64,64), (0, y), (w, y), 1)
-
     pg.display.update()
 
 def main():
     screen = init()
+    grid = g.Grid(5,5)
+    grid.print_grid()
 
     while not finished:
         handle_events()
-        draw_grid(screen)
 
     pg.quit()
 
